e7e56bd445b89084205a4ac836cce85d2ed27958
Audrey CHEVRON <achevron@bess-f4r6s3.clusters.42paris.fr>
2024-11-18 12:36:24 +0100

    text_id init

--- srcs/select_texture.c
+++ srcs/select_texture.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   select_texture.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tchalaou <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: achevron <achevron@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/23 16:01:19 by tchalaou          #+#    #+#             */
-/*   Updated: 2024/10/18 17:24:13 by tchalaou         ###   ########.fr       */
+/*   Updated: 2024/11/18 12:35:53 by achevron         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@ int	select_texture(float dir, int ori)
 {
 	int	tex_id;
 
+	tex_id = 0;
 	if ((dir >= 1 && dir < 1.5 && ori) || (dir >= 1.5 && dir < 2 && ori))
 		tex_id = 0;
 	else if ((dir >= 0 && dir < 0.5 && ori) || (dir >= 0.5 && dir < 1 && ori))
